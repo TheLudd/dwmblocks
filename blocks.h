e836e82d2b3aef36f97375132dd072eb7388e254
Ludwig Magnusson <ludwig@mediatool.com>
2021-07-06 21:41:22 +0200

    add light config

--- blocks.h
+++ blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "cat /sys/class/power_supply/BAT0/capacity | xargs printf ' %q%%'" , 60, 1},
+	{"", "light | xargs printf '%3s%s%%'" , 0, 2},
+	{"", "date '+%a %b %d %H:%M%p '",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
