f9dd129030f2b5f34326d54049ae7acd1135f65d
Ludwig Magnusson <ludwig@mediatool.com>
2024-03-07 09:58:14 +0100

    add check for dotfiles changes

--- blocks.h
+++ blocks.h
@@ -1,9 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat /sys/class/power_supply/BAT0/capacity | xargs printf ' %q%%'" , 60, 1},
-	{"", "light | xargs printf '%3s%s%%'" , 0, 2},
-	{"", "date '+%a %b %d %H:%M%p '",					5,		0},
+	{"", "count-pacman-upgrades" , 60 * 60 * 5, 3},
+	{"", "check-dotfiles-repo" , 60 * 60, 4},
+	{"", "battery" , 60, 1},
+	{"", "light" , 0, 2},
+	{"", "date '+%a %b %d %H:%M%p'",					5,		0},
+	{"", "count-dotfiles bar" , 3600, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
