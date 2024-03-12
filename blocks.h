f69360e1d7a4c32b4589bb8ad6fe24e9d279be8f
Ludwig Magnusson <ludwig@mediatool.com>
2024-03-12 22:43:07 +0100

    add display sound

--- blocks.h
+++ blocks.h
@@ -4,6 +4,7 @@ static const Block blocks[] = {
 	{"", "count-pacman-upgrades" , 60 * 60 * 5, 3},
 	{"", "check-dotfiles-repo" , 60 * 60, 4},
 	{"", "battery" , 60, 1},
+	{"", "display-sound" , 0, 6},
 	{"", "light" , 0, 2},
 	{"", "date '+%a %b %d %H:%M%p'",					5,		0},
 	{"", "count-dotfiles bar" , 3600, 0},
