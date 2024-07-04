e7bb1cedd43bc8b542360d0ae6da2a8fd32bb941
Ludwig Magnusson <ludwig@mediatool.com>
2024-07-04 12:37:52 +0200

    reorder some icons

--- blocks.h
+++ blocks.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "count-pacman-upgrades" , 60 * 60 * 5, 3},
 	{"", "check-dotfiles-repo" , 60 * 60, 4},
+	{"", "count-pacman-upgrades" , 60 * 60 * 5, 3},
+	{"", "display-sound" , 3500, 6},
 	{"", "battery" , 60, 1},
-	{"", "display-sound" , 0, 6},
 	{"", "light" , 0, 2},
 	{"", "date '+%a %b %d %H:%M%p'",					5,		0},
 	{"", "count-dotfiles bar" , 3600, 0},
